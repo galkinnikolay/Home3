2a480b61ecb1148604ebe29ee41094700483b86a
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-16 18:01:08 +0300

    задача 13

--- Zadacha13/Program.cs
+++ Zadacha13/Program.cs
@@ -14,17 +14,22 @@ Console.WriteLine("Введите число: ");
 string text = Console.ReadLine();
 int number = Convert.ToInt32(text);
 
-if (number < 100)
+if (number > 0 && number < 99)
 {
 Console.WriteLine("Третьей цифры нету");
 }
-else if (number < 999)
+else if (number > 100 && number < 999)
 {
-int t = (number % 10);
-Console.WriteLine("Вывод числа: " + t);
+int t100 = (number % 10);
+Console.WriteLine("Вывод числа: " + t100);
 }
-else if (number > 1000)
+else if (number > 1000 && number < 9999)
 {
-int t = ((number / 100)%10);
-Console.WriteLine("Вывод числа: " + t);
+int t1000 = ((number / 10)%10);
+Console.WriteLine("Вывод числа: " + t1000);
+}
+else if (number > 10000 && number < 99999)
+{
+int t10000 = ((number / 100)%10);
+Console.WriteLine("Вывод числа: " + t10000);
 }
